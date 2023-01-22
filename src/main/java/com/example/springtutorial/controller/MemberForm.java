01d61b2745fcc2de60c22395a58414449c86211b
episohe <maybe_be@naver.com>
2023-01-22 20:00:04 +0900

    회원 관리 예제 - 웹 MVC개발

---
+++ src/main/java/com/example/springtutorial/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package com.example.springtutorial.controller;
+
+public class MemberForm {
+
+    private String name;
+
+    private String getName() {
+        return name;
+    }
+
+    public String setName() {
+        this.name = name;
+        return name;
+    }
+}
